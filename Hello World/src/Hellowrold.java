9aab6f48717307e8372fe414a1d4777fdcb7beb4
inchulemail <whdlscjf97@naver.com>
2020-04-13 11:02:01 +0900

    add print line

--- Hello World/src/Hellowrold.java
+++ Hello World/src/Hellowrold.java
@@ -4,6 +4,7 @@ public class Hellowrold {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hellow world");
+		System.out.println("Hellow  his world");
 	}
 
 }
