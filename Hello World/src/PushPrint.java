4fada9890753cf15e03c4b06ea8f9e7243c48bf4
whdlscjf97 <63217165+whdlscjf97@users.noreply.github.com>
2020-04-16 12:54:15 +0900

    Update PushPrint.java
    PushPrint.java modified on GitHub

--- Hello World/src/PushPrint.java
+++ Hello World/src/PushPrint.java
@@ -2,8 +2,8 @@
 public class PushPrint {
 
 	public static void main(String[] args) {
-	System.out.println("Add Java file");
-
+		System.out.println("Add Java file");
+		System.out.println("Hello");
 	}
 
 }
